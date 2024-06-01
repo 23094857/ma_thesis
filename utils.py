6ebcad2c1f1db337db641052e2797050966038ab
23094857 <github2934587@gmail.com>
2024-06-01 19:02:20 +0200

    Delete utils.py

--- utils.py
+++
@@ -1,4 +0,0 @@
-def dict_has_at_least_two_nonzero_values(probabilities: dict) -> bool:
-    """Return True if there are at least two non-zero entries in the dictionary."""
-    non_zero_count = sum(1 for value in probabilities.values() if value > 0)
-    return non_zero_count >= 2
